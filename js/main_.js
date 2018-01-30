5088780f13483a7a88c67a0c6fcb6afededd90d9
kuipou <kuipou@gmail.com>
2018-01-30 09:45:02 -0500

    Started testing the array

--- js/main_.js
+++ js/main_.js
@@ -5,18 +5,37 @@
 
 
 // declare var now 
-var currentPage, maxItems, DOMstrings, studentList, totalPages;
+let currentPage, maxItems, DOMstrings, studentList, totalPages;
 console.log("App started");
 
-currentPage = 1;
-maxItems = 10;
+function init() {
+    currentPage = 1;
+    maxItems = 10;
+    // Store CSS class name in object for easier use later
+    DOMstrings = {
+        studentContainer: ".student-list",
+        studentItem: ".student-item",
 
-// Store CSS class name in object for easier use later
-DOMstrings = {
-    studentContainer: ".student-list",
-    studentItem: ".student-item",
-    
-};
+    };
+}
+
+init();
+
+
+var testarr = [];
+for( let i = 0; i < 54; i++) {
+    testarr.push(i);
+}
+
+// for the current page ex 6 -1 =5 * the number of items is our counter
+
+for (i = (currentPage - 1) * maxItems; i < currentPage * maxItems; i++) {
+    if (i < testarr.length) {
+        console.log(testarr[i]);
+    }
+}
+
+
 // Convert NodeList into Array
 studentList= [].slice.call(document.querySelectorAll(DOMstrings.studentItem));
 
@@ -24,7 +43,14 @@ studentList= [].slice.call(document.querySelectorAll(DOMstrings.studentItem));
 totalPages = Math.ceil(studentList.length / maxItems);
 
 
-function showPage() {
+// function showPage(/* arguments here for page number and student list */) {
+//     // first hide all students on the page
+//     // Then loop through all students in our student list argument
+//     // if student should be on this page number
+//     // show the student
+// }
+
+function showPage(current, stdent) {
     console.log(DOMstrings.studentContainer);
     // go fetch the container
     var innerList = document.querySelector(DOMstrings.studentContainer);
@@ -35,10 +61,26 @@ function showPage() {
     // Loop through
     console.log(Array.isArray(studentList));
 
-    for (var i = 0; i < maxItems; i++) {
-        innerList.createElement( studentList[i]);
+    for (i = (currentPage - 1) * maxItems; i < currentPage * maxItems; i++) {
+        if (i < testarr.length) {
+        innerList.append( studentList[i]);}
     }
 }
+function pagination(params) {
+    
+}
+function appendPageLinks(/* take a student list as an argument */) {
+    // determine how many pages for this student list 
+    // create a page link section
+    // “for” every page
+    // add a page link to the page link section
+    // remove the old page link section from the site
+    // append our new page link section to the site
+    // define what happens when you click a link
+    // Use the showPage function to display the page for the link clicked
+    // mark that link as “active”
+}
+
 
 showPage();
 console.log(`The total pages is ${totalPages}`);
