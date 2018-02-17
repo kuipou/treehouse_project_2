a1e3581c345ff8e4cbb05546817e5c0154ed1145
kuipou <kuipou@gmail.com>
2018-02-16 23:55:52 -0500

    Fixed the pagination though not finished with comments

--- js/main_.js
+++ js/main_.js
@@ -11,6 +11,7 @@ console.log("App started");
 function init() {
     currentPage = 1;
     maxItems = 10;
+
     // Store CSS class name in object for easier use later
     DOMstrings = {
         studentContainer: ".student-list",
@@ -23,21 +24,31 @@ init();
 
 
 var testarr = [];
-for( let i = 0; i < 54; i++) {
+for (let i = 0; i < 54; i++) {
     testarr.push(i);
 }
 
 // for the current page ex 6 -1 =5 * the number of items is our counter
-
 for (i = (currentPage - 1) * maxItems; i < currentPage * maxItems; i++) {
     if (i < testarr.length) {
         console.log(testarr[i]);
     }
 }
+function activePage(current) {
+    let activePagination = document.querySelectorAll(".pagination ul li a");
 
+    for (let i = 0; i <= activePagination.length; i++) {
+        console.log(activePagination[i]);
+        if (activePagination[i].classList.contains("active")) {
+            console.log("was true add active");
+            activePagination[i].classList.remove("active");
+        }
+        activePagination[current - 1].classList.add("active");
+    }
+}
 
 // Convert NodeList into Array
-studentList= [].slice.call(document.querySelectorAll(DOMstrings.studentItem));
+studentList = [].slice.call(document.querySelectorAll(DOMstrings.studentItem));
 
 // The total page is calculated by dividing the lenght of the Array divided by the max item (54 /10)
 totalPages = Math.ceil(studentList.length / maxItems);
@@ -56,19 +67,57 @@ function showPage(current, stdent) {
     var innerList = document.querySelector(DOMstrings.studentContainer);
     console.log(innerList);
     // empty the list
-    innerList.innerHTML = ""; 
+    innerList.innerHTML = "";
     console.log(innerList);
     // Loop through
-    console.log(Array.isArray(studentList));
+    // console.log(Array.isArray(studentList));
 
-    for (i = (currentPage - 1) * maxItems; i < currentPage * maxItems; i++) {
+    for (i = (current - 1) * maxItems; i < current * maxItems; i++) {
         if (i < testarr.length) {
-        innerList.append( studentList[i]);}
+            innerList.append(stdent[i]);
+        }
     }
+
 }
-function pagination(params) {
-    
+function pagination() {
+    let pagination = document.createElement('div');
+    pagination.className = "pagination";
+    // let paginationList = document.createElement('ul');
+    let paginationLi = "";
+
+    for (let pages = 1; pages <= totalPages; pages++) {
+        if (pages === currentPage) { paginationLi += `<li><a class="active" href="#">${pages}</a></li>`; }
+        else { paginationLi += `<li><a href="#">${pages}</a></li>`; }
+
+    }
+    pagination.innerHTML = `<ul>${paginationLi}</ul>`;
+
+    document.querySelector(".page").appendChild(pagination);
+    var pgni = document.querySelectorAll(".pagination ul li a");
+
+    for (let i = 0; i < totalPages; i++) {
+        // if 
+        console.log("item contains active : " + pgni[i].classList.contains("active"));
+
+        pgni[i].addEventListener("click", function (event) {
+            // remove once to make sure that theres no active class
+            console.log("li was clicked with a target of " + event.target.textContent);
+            console.log("clicked " + parseInt(event.target.parentNode.className));
+            showPage(parseInt(event.target.textContent), studentList);
+            activePage(parseInt(event.target.textContent);
+
+        });
+    }
+    // document.querySelector(".pagination ul li a").addEventListener("click", function (event) {
+    //     console.log("li was clicked with a target of " + event.target.innerHTML);
+    //     console.log("clicked");
+    // });
+
+
+
 }
+pagination();
+
 function appendPageLinks(/* take a student list as an argument */) {
     // determine how many pages for this student list 
     // create a page link section
@@ -82,5 +131,5 @@ function appendPageLinks(/* take a student list as an argument */) {
 }
 
 
-showPage();
+showPage(1, studentList);
 console.log(`The total pages is ${totalPages}`);
